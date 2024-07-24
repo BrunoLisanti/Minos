3fc3dafbd95e912972b1fbb205d5bd37a5d42d7f
cc-dan <dantecocciolo@gmail.com>
2024-07-24 17:00:32 -0300

    - Cambios en los controles para que sean estilo Doom - Modificaciones al movimiento del jugador. Diferencia en inercia, velocidad disminuida cuando el jugador está retrocediendo. - Se eliminó el head bobbing por el momento - Se eliminó la captura del cursor - Obstaculos en la escena principal para probar colisiones - Se eliminó código referente a la diferencia de control durante un salto (mecánica que no se va a utilizar)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
+const SPEED = 6.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
 
@@ -15,13 +15,13 @@ var gravity = 9.8
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#func _ready():
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _unhandled_input(event): #Movimiento de camara
-	if event is InputEventMouseMotion:
-		
-		head.rotate_y(-event.relative.x * SENSITIVITY)
+#func _unhandled_input(event): #Movimiento de camara
+	#if event is InputEventMouseMotion:
+		#
+		#head.rotate_y(-event.relative.x * SENSITIVITY)
 		#camera.rotate_x(-event.relative.y * SENSITIVITY) #Mover verticalmente la camara
 		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60)) #Limitar el movimiento vertical de la camara
 
@@ -39,28 +39,23 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("rotate_left"):
 		head.rotate_y(SENSITIVITY * 10)
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if is_on_floor():
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
-		else:
-			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 7.0)
-	else:
-		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 2.0)
-		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 2.0)
-		camera.transform.origin.y = lerp(camera.transform.origin.y, 0.0, 0.1)	
 		
-	t_bob += delta * velocity.length() * float(is_on_floor())
+	# Get the input direction and handle the movement/deceleration.
+	# Movimiento en el eje Z de acuerdo a la dirección en la que se está mirando.
+	var input_dir = (1 if Input.is_action_pressed("down") else 0) - (1 if Input.is_action_pressed("up") else 0) # 1 si se presiona abajo, -1 si se presiona arriba. Cero si se presionan ambos.
+	var direction = (head.transform.basis * Vector3(0, 0, input_dir)).normalized()
+	var current_speed = SPEED if input_dir == -1 else SPEED / 2
 	
-	if direction:
-		camera.transform.origin = _headbob(t_bob)
-	else:
-		camera.transform.origin.y = lerp(camera.transform.origin.y, 0.0, 0.1)
+	velocity.x = lerp(velocity.x, direction.x * current_speed, delta * 6.0)
+	velocity.z = lerp(velocity.z, direction.z * current_speed, delta * 6.0)	
+		
+	# Head bobbing
+	#t_bob += delta * velocity.length() * float(is_on_floor())
+	#
+	#if direction:
+		#camera.transform.origin = _headbob(t_bob)
+	#else:
+		#camera.transform.origin.y = lerp(camera.transform.origin.y, 0.0, 0.1)
 	
 	move_and_slide()
 	
