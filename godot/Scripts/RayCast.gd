c6e8a309e5cae26c38597faa05316941c7fd92aa
BrunoLisanti <brunodavidlisanti@gmail.com>
2024-08-12 21:57:01 -0300

    Agregada mecanica para llevar el cajon junto a su escena. Modificada la escena del jugador para agregarle un nodo que represente la posicion en que llevara el cajon.

---
+++ godot/Scripts/RayCast.gd
@@ -0,0 +1,21 @@
+extends RayCast3D
+
+var obj = null
+@onready var hold_position = $"../HoldPosition"
+@onready var head = $"../.."
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("interact"):
+		if obj == null:
+			var collider = get_collider()
+			if collider != null:
+				if collider.is_in_group("pickable"):
+					obj = collider
+		else:
+			obj = null
+
+	if obj != null:
+		obj.position = hold_position.global_position
+		obj.rotation = head.rotation
+
