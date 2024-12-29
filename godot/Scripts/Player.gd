9a7db36d9ea115880aab6ca23b03e1d32feca7b0
cc-dan <dantecocciolo@gmail.com>
2024-12-28 21:20:15 -0300

    Objetivos que desaparecen al colisionar con ellos mientras se tiene el cajón en manos. Mejorado el código de agarrar el cajón: se borra el objeto del mundo y se vuelve a instanciar cuando se suelta.

--- godot/Scripts/Player.gd
+++ godot/Scripts/Player.gd
@@ -15,32 +15,36 @@ var step_time: float = 0
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera
-@onready var box_object: Node3D = null
+@onready var box: Resource = preload("res://Scenes/Cajon.tscn")
 @onready var viewmodel_camera: Camera3D = $Control/BoxViewportContainer/SubViewport/BoxCamera
 @onready var box_viewmodel: MeshInstance3D = $BoxViewmodel
 @onready var footsteps_pool: Node = $FootstepsPool
 @onready var floor_raycast: RayCast3D = $FloorRaycast
+@onready var interaction_range: Area3D = $InteractionArea
+@onready var world: Node = get_parent()
 
 @onready var movement_component: MovementComponent = $MovementComponent
+
+var carrying: bool = false
 
 func _process(_delta):
 	viewmodel_camera.global_transform = camera.global_transform
 	
 	if Input.is_action_just_pressed("interact"):
-		if box_object == null:
-			# pick up
-			var object := floor_raycast.get_collider()
-			if object != null && object.is_in_group("pickable"):
-				box_object = object
-				box_object.visible = false
-				box_viewmodel.visible = true
+		if !carrying:
+			for body in interaction_range.get_overlapping_bodies():
+				if (body.is_in_group("pickable")):
+					body.queue_free()
+					carrying = true
+					break
 		else:
-			# drop
-			box_object.global_position = global_position + Vector3.UP + -transform.basis.z
-			box_object.rotation = rotation
-			box_object.visible = true
-			box_object = null
-			box_viewmodel.visible = false;
+			var box_instance: Node = box.instantiate()
+			world.add_child(box_instance)
+			box_instance.global_position = global_position + Vector3.UP + -transform.basis.z
+			box_instance.rotation = rotation
+			carrying = false
+			
+		box_viewmodel.visible = carrying
 
 func _physics_process(delta):	
 	var y_rotation := (1 if Input.is_action_pressed("rotate_left") else 0) - (1 if Input.is_action_pressed("rotate_right") else 0) # 0, 1 o -1 de acuerdo a qué teclas estén apretadas.
@@ -48,7 +52,7 @@ func _physics_process(delta):
 	
 	var z_direction := (1 if Input.is_action_pressed("backward") else 0) - (1 if Input.is_action_pressed("forward") else 0)
 	var direction := Vector3(0, 0, z_direction)
-	var speed := SPEED if box_object == null else SPEED / 1.5
+	var speed := SPEED if !carrying else SPEED / 1.5
 	speed = speed if direction == Vector3.FORWARD else speed / 2
 	
 	var hovering := not is_on_floor()
@@ -67,3 +71,7 @@ func _physics_process(delta):
 	t_bob += speed * delta # Acumulador de tiempo usado como oscilador, y teniendo en cuenta la velocidad (?)
 	var bob := Vector3(cos(t_bob * BOB_FREQ / 1.8) * BOB_AMP_HORIZONTAL, sin(t_bob * BOB_FREQ) * BOB_AMP_VERTICAL, 0)
 	camera.transform.origin = bob if walking else lerp(camera.transform.origin, Vector3.ZERO, .1)
+
+func _on_interaction_area_body_entered(body: Node3D)->void:
+	if body.is_in_group("objective") && carrying:
+		body.queue_free()
