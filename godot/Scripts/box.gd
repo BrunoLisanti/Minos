1da6c550b6dc58ddbec429541ef5273caa756ab6
Bruno <brunodavidlisanti@gmail.com>
2025-01-07 21:02:55 -0300

    Resuelto un error que hacia que si la caja estuviera tocando paredes a la vez que colisiona contra el piso, la colision no funcionaba correctamente, eso debido a la propiedad "max_contacts" que establece cuantos contactos (NO COLISIONES) simultaneos pueda detectar el objeto, 16 son suficientes. Se hizo que la caja no se pueda agarrar hasta que el sonido de golpear contra el suelo termine de reproducirse, debido a que si el nodo de la caja se elimina al agarrarlo, se corta abruptamente el sonido, de esta manera nos aseguramos que eso no ocurra

--- godot/Scripts/box.gd
+++ godot/Scripts/box.gd
@@ -1,11 +1,14 @@
 extends RigidBody3D
 var _is_on_floor := false
 @onready var fall_sound := $FallSound
+@onready var map := get_parent()
 
 func _on_body_entered(body: Node) -> void:
-	if body.name == "FloorCollision":
-		_is_on_floor = true
+	if !body.is_in_group("wall"):
 		fall_sound.play(false)
-	
+		
 func is_on_floor() -> bool:
 	return _is_on_floor
+
+func _on_fall_sound_finished() -> void:
+	_is_on_floor = true
