f40830eacf592b0ae2fd03fac69bc9da247dea9d
Bruno <brunodavidlisanti@gmail.com>
2025-02-02 21:05:49 -0300

    Menu principal

---
+++ godot/Scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var start_btn: Button = find_child("Start")
+@onready var options_btn: Button = find_child("Options")
+@onready var quit_btn: Button = find_child("Exit")
+
+func _ready() -> void:
+	start_btn.pressed.connect(start)
+	quit_btn.pressed.connect(get_tree().quit)
+	options_btn.pressed.connect(options)
+	
+func start() -> void:
+	get_tree().change_scene_to_file("res://Scenes/world3.tscn")
+
+func options() -> void:
+	print("Options")
+
+	
--- godot/Scripts/pause_menu.gd
+++ godot/Scripts/pause_menu.gd
@@ -2,11 +2,17 @@ extends ColorRect
 
 @onready var resume_btn: Button = find_child("Resume")
 @onready var quit_btn: Button = find_child("Quit")
+@onready var main_menu_btn: Button = find_child("MainMenu")
 
 func _ready() -> void:
 	hide()
 	resume_btn.pressed.connect(unpause)
 	quit_btn.pressed.connect(get_tree().quit)
+	main_menu_btn.pressed.connect(main_menu)
+
+func main_menu():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func unpause():
 	hide()
