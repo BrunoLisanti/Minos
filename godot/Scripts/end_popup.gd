a1716ce73016dc6a21e12c40777159ae361c1d86
dante <dantecocciolo@gmail.com>
2025-06-20 03:19:33 -0300

    Debuggers eliminados, agregado label de créditos en el menú principal.

--- godot/Scripts/end_popup.gd
+++ godot/Scripts/end_popup.gd
@@ -1,11 +1,9 @@
 extends ColorRect
 
 func _on_exit_pressed() -> void:
-	print("test")
 	get_tree().quit()
 
 func _on_continue_pressed() -> void:
-	print("test")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_node("../..").can_pause = true
 	get_tree().paused = false
@@ -16,7 +14,3 @@ func enable()->void:
 	get_node("../..").can_pause = false
 	get_tree().paused = true
 	visible = true
-
-
-func _on_resume_pressed() -> void:
-	print("test")
